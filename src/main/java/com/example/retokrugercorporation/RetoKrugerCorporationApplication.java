e0526ba981e957d3ba04a6a42457967cb23c750d
DavSeb21031990 <72324297+DavSeb21031990@users.noreply.github.com>
2022-08-26 21:55:20 -0500

    Inicio proyecto

---
+++ src/main/java/com/example/retokrugercorporation/RetoKrugerCorporationApplication.java
@@ -0,0 +1,13 @@
+package com.example.retokrugercorporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoKrugerCorporationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoKrugerCorporationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/retokrugercorporation/RetoKrugerCorporationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retokrugercorporation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoKrugerCorporationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
